3e81401a02ddd9b285dbf0835e6ce219ae0825da
Cendevs <cndigitech@gmail.com>
2021-10-16 09:41:22 +0100

    Add create method with welcome email logic

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,23 @@
 class RegistrationsController < Devise::RegistrationsController
+    #skip_before_action :verify_authenticity_token, only: %i[create new]
+
+    def create
+        super
+        if @user.persisted?
+            #begin
+            UserMailer.with(user: @user).welcome_email.deliver_now
+            #rescue
+                #flash.notice = "Successfully signed up. (Welcome email not sent)"
+            #end
+        end
+    end
     private
 
     def sign_up_params
-        params.require(:user).permit(:firstname, :surename, :gender, :email, :password, :password_confirmation)
+        params.require(:user).permit(:firstname, :surename, :gender, :avatar, :email, :password, :password_confirmation)
     end
 
     def account_update_params
-        params.require(:user).permit(:firstname, :surename, :gender, :email, :password, :password_confirmation, :current_password)
+        params.require(:user).permit(:firstname, :surename, :gender, :avatar, :email, :password, :password_confirmation, :current_password)
     end
 end
