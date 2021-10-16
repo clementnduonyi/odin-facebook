5807b98d3335f93c308de4b83f41465bee6b9627
Cendevs <cndigitech@gmail.com>
2021-10-16 09:34:08 +0100

    Enable user avater on the new devise user sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permited_parameters,
+        if: :devise_controller?
+    
     def after_sign_in_path_for(resource_or_scope)
         posts_path
     end
@@ -6,4 +9,17 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(resource_or_scope)
         root_path
     end
+
+    protected
+
+    def configure_permited_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    end
+
+    #public
+
+    
+
+    
 end
