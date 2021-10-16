9cf49043eafb15330d530143a8ae7a0fcddab1ac
Cendevs <cndigitech@gmail.com>
2021-10-16 09:43:07 +0100

    modify requests and friends page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!, only: [:show, :index]
+    # before_action :set_request
+    
     
     def index
         @users = User.all
-        
+        @requests = Request.all
+        @requests.each do |request|
+            request
+        end
     end
 
     
     def show
+        @users = User.all
         if params[:id]
             @user = User.find(params[:id])
         else
@@ -15,6 +21,14 @@ class UsersController < ApplicationController
         end
         @requests = current_user.recieved_requests.all
         @fans = current_user.fans.all
+        @posts = Post.all
+        
     end
 
+    def friends
+        @fans = current_user.fans.all
+        # @post = @fans.posts.all
+    end
+
+    
 end
